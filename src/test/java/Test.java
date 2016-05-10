d1eacea6ce6191cb660d05ee22ce5c8c4ff1a7ff
Rafael Villar Villar <rafael@villar.me>
2016-05-11 06:30:41 +1000

    Testing with Apache commons Net as replacement for javamail

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,11 +2,15 @@ import javax.mail.Folder;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.URLName;
+import java.io.IOException;
+import java.net.Inet4Address;
 import java.util.Base64;
 import java.util.Properties;
 
 import com.sun.mail.imap.*;
 import org.apache.commons.codec.digest.Crypt;
+import org.apache.commons.net.imap.IMAPSClient;
+import org.apache.commons.net.pop3.POP3MessageInfo;
 
 public class Test {
 
@@ -24,9 +28,42 @@ public class Test {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main3(String[] args) {
 		byte[] raw = new byte[] {0, 0, 63};
 		System.out.println(">> " + Base64.getUrlEncoder().encodeToString(raw));
 	}
 
+	public static void main(String[] args) throws IOException {
+
+		IMAPSClient client = new IMAPSClient("SSL", true);
+
+		client.connect("mail.villar.id.au");
+		System.out.println("<<< CONNECT");
+		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+		client.login("", "");
+		System.out.println("<<< LOGIN");
+		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+		client.select("INBOX.Archivo");
+		System.out.println("<<< SELECT");
+		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+//		client.select("Archivo");
+//		System.out.println("<<< SELECT");
+//		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+		client.list("\"\"", "\"INBOX.*\"");
+		System.out.println("<<< LIST");
+		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+//		client.fetch("1:136", "(FLAGS BODY[HEADER.FIELDS (DATE SUBJECT FROM)])");
+//		System.out.println("<<< FETCH");
+//		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+		client.disconnect();
+		System.out.println("<<< DISCONNECT");
+		for(String reply: client.getReplyStrings()) System.out.println(">>> " + reply);
+
+	}
 }
